31a42110c533b8050be5b9f3dbeb1eda3d12a0b9
zmz125000 <zlx220000@outlook.com>
2026-04-09 12:03:34 +0800

    Create H.265 10-bit encoder header

---
+++ h265_10bit_encoder.h
@@ -0,0 +1,9 @@
+# H.265 10-bit Encoder
+
+// Configuration for H.265 10-bit encoding with P010 pixel format and support for HDR
+
+class H265_10bit_Encoder {
+public:
+    void encode(const Frame& frame);
+    // Additional methods and configurations specific to 10-bit encoding
+};
